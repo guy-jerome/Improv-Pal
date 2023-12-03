fa3b7f30ef745ed72493957aac80b13adebed66a
guy-jerome <aaronjamesroberts@gmail.com>
2023-12-03 16:46:24 -0600

    Added the basic button logic for the improv Pal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 
-function App() {
+export default function App() {
 
+  const [message,setMessage] = useState("");
+  const [response,setResponse] = useState("");
+
+  function sendMessage(){
+    setMessage("")
+    setResponse(message)
+  }
+
+  function textChanged(event){
+    setMessage(event.target.value)
+  }
 
   return (
-    <>
-
-    </>
-  )
+      <>
+        <h1>Improve Buddy</h1>
+        <textarea value={response} readOnly></textarea>
+        <input type="text" value={message} onChange={textChanged}/>
+        <button onClick={sendMessage}>Send Message</button>
+      </>
+    )
 }
 
-export default App
+
